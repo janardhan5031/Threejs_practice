75caa4428272324eb5e5225accb13a95924661a0
janardhan5031 <bjanardhan5031@gmail.com>
2023-11-28 21:48:18 +0530

    model rotation activated

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(){
 
   return (
     <>
-      <div className='bg-red-500 text-white  text-center p-4'> Hello word</div>
+      {/* <div className='bg-red-500 text-white  text-center p-4'> Hello word</div> */}
       {/* <CubeComponent /> */}
       {/* <SphereComponent />f */}
       {/* <MapComponent /> */}
--- src/components/MuseumComponent.tsx
+++ src/components/MuseumComponent.tsx
@@ -1,86 +1,81 @@
 import React, { useEffect, useRef, useState } from "react";
-import * as THREE from 'three'
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import {GUI} from 'dat.gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import {ThreeDots} from 'react-loader-spinner'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { ThreeDots } from "react-loader-spinner";
 
+let firstRound;
+let secondRound;
+let thirdRound;
 
 const MuseumComponent: React.FC = () => {
   const mount1 = useRef(null);
-  const [isLoading,setLoading]= useState(true)
-  const [cubeRotation, setCubeRotation] = useState(1);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
+    const scene = new THREE.Scene();
+    scene.add(new THREE.AxesHelper(25))
 
-    const scene = new THREE.Scene()
-    // const axesHelper = new THREE.AxesHelper(5)
-    // scene.add(axesHelper)
+    // scene.rotation.y=45;
+    // scene.position.y=0;
+    // scene.position.z=0;
 
-    // const light = new THREE.PointLight(0xffffff, 100)
-    // light.position.set(0,1,10)
-    // scene.add(light)
 
     const camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-    )
-    camera.position.x = 20
-    camera.position.y = 15
-    camera.position.z = 20
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
+    const renderer = new THREE.WebGLRenderer();
+    // renderer.rotation.y=10
 
-    const renderer = new THREE.WebGLRenderer()
-    renderer.useLegacyLights = false 
-    renderer.shadowMap.enabled = true
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    mount1.current.appendChild(renderer.domElement)
+    renderer.useLegacyLights = false;
+    renderer.shadowMap.enabled = true;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mount1.current.appendChild(renderer.domElement);
 
-    const controls = new OrbitControls(camera, renderer.domElement)
-    controls.enableDamping = true
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
     controls.minPolarAngle = 0;
-    controls.maxPolarAngle =Math.PI/2.1 ; // Set the maximum polar angle to Math.PI/2 (90 degrees)
+    controls.maxPolarAngle = Math.PI / 2.1;
     controls.minDistance = 4;
 
-
-    const loader = new GLTFLoader()
+    const loader = new GLTFLoader();
     loader.load(
-        'https://testbucketsq.s3.ap-south-1.amazonaws.com/GLTF_Models/MuseumBakedA.glb',
-        function (gltf) {
-            gltf.scene.traverse(function (child) {
-                // if ((child as THREE.Mesh).isMesh) {
-                //     const m = child as THREE.Mesh
-                //     m.receiveShadow = true
-                //     m.castShadow = true
-                // }
-                // if ((child as THREE.Light).isLight) {
-                //     const l = child as THREE.SpotLight
-                //     l.castShadow = true
-                //     l.shadow.bias = -0.003
-                //     l.shadow.mapSize.width = 2048
-                //     l.shadow.mapSize.height = 2048
-                // }
-            })
-            scene.add(gltf.scene)
-            setLoading(false)
-        },
-        (xhr) => {
-            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-        },
-        (error) => {
-            console.log(error)
-        }
-    )
-
-    scene.rotation.y=90
+      "https://testbucketsq.s3.ap-south-1.amazonaws.com/GLTF_Models/MuseumBakedA.glb",
+      function (gltf) {
+        gltf.scene.traverse(function (child) {});
+        scene.add(gltf.scene);
+        setLoading(false);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
     const animate = () => {
-      requestAnimationFrame(animate);
+      requestAnimationFrame(animate);   // continously call animate function to render
 
-      setCubeRotation(v=>v+0.01)
       controls.update();
+      
+      // Rotate the camera around the model in the y-axis  
+
+      const speed = 0.0001; // Adjust the rotation speed
+      let angle = performance.now() * speed;
+
+      console.log(angle)
+
+      if(angle<5.2){
+        const radius = 25; // Adjust the radius of the camera orbit
+        camera.position.x = scene.position.x + radius * Math.sin(angle);
+        camera.position.z = scene.position.z + radius * Math.cos(angle);
+        camera.lookAt(scene.position);
+      }
 
       render();
     };
@@ -88,30 +83,28 @@ const MuseumComponent: React.FC = () => {
     const render = () => {
       renderer.render(scene, camera);
     };
-    
+
     animate();
 
-    // clear the previously appended canvas elements
     return () => {
       mount1.current?.removeChild(renderer.domElement);
     };
-
   }, []);
 
   return (
     <div className="flex items-center justify-center bg-black">
-        {
-            isLoading && <ThreeDots 
-                height="80" 
-                width="80" 
-                radius="9"
-                color="#4fa94d" 
-                ariaLabel="three-dots-loading"
-                wrapperStyle={{position:'fixed'}}
-                visible={true}
-             />
-        }
-        <div ref={mount1}></div>
+      {isLoading && (
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#4fa94d"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{ position: "fixed" }}
+          visible={true}
+        />
+      )}
+      <div ref={mount1}></div>
     </div>
   );
 };
